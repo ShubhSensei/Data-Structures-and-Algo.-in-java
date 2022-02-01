52175e2b79dc430e9239bd12b1b263465d5e9053
Shubham Bansal <bansal4995@gmail.com>
2022-02-01 11:27:44 +0530

    String Questions

--- Strings/E_01.java
+++ Strings/E_01.java
@@ -1,18 +1,22 @@
 // package Assignments.Strings;
-// // https://leetcode.com/problems/defanging-an-ip-address/
-// public class E_01 {
-//     public static void main(String[] args) {
-//         String s = ".";
-//         String address = "1.1.1.1";
-//         for (int i = 0; i < address.length(); i++) { 
-//             char ch = address.charAt(i);
-//             String b = "address.charAt(i)";
-//             StringBuilder a = new StringBuilder();
-//             a.append(ch);
-//             if(a == ){
-//                 a = "[.] ";
-//             }
-//         }
-//         System.out.println(address.length());
-//     }
-// }
+// https://leetcode.com/problems/defanging-an-ip-address/
+public class E_01 {
+    public static void main(String[] args) {
+        String s = ".";
+        String address = "255.100.50.0";
+        StringBuilder a = new StringBuilder();
+        for (int i = 0; i < address.length(); i++) { 
+            char ch = address.charAt(i);
+             if(ch == '.'){
+                 s = "[.]";
+                 a.append(s);
+             } else{
+                a.append(ch);
+             }
+        }
+        System.out.print(a);        
+    }
+}
+                        /*In Function, STring builder can be returned by using the method "Var_name.toString();"
+                     Eg.-  In this case 
+                            a.toString(); */

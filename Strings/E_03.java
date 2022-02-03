ac8bfa012ba0bb6ed0a32e5aa93cd9140fc58094
Shubham Bansal <bansal4995@gmail.com>
2022-02-03 13:09:59 +0530

    String Questions

---
+++ Strings/E_03.java
@@ -0,0 +1,21 @@
+public class E_03 {
+    public static void main(String[] args) {
+        String command = "G()(al)";
+        StringBuilder b = new StringBuilder();
+        for (int i = 0; i < command.length(); i++) {
+            char ch = command .charAt(i);
+            if(ch =='G'){
+                b.append(ch);
+            } else if(ch == '('){
+                i++;
+                ch = command .charAt(i);
+                if(ch == ')'){
+                    b.append('o');
+                } else{
+                    b.append("al");
+                }
+            }
+        }
+        System.out.println(b);
+    }
+}

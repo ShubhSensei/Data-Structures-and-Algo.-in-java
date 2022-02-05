ddd29ef76f63604113b07cfb984503f930a96af3
Shubham Bansal <bansal4995@gmail.com>
2022-02-05 15:25:46 +0530

    String Questions

---
+++ Strings/E_07.java
@@ -0,0 +1,17 @@
+package Assignments.Strings;
+
+public class E_07 {
+    public static void main(String[] args) {
+        String s = "HELLO";
+        String ans = toLowerCase(s);
+        System.out.println(ans);
+    }
+    static String toLowerCase(String s) {
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.toLowerCase(). charAt(i);
+            ans.append(ch);
+        }
+        return ans.toString();
+    }
+}
---
+++ Strings/E_08.java
@@ -0,0 +1,30 @@
+package Assignments.Strings;
+
+public class E_08 {
+    public static void main(String[] args) {
+        String s = "book";
+        boolean ans = halvesAreAlike(s);
+        System.out.println(ans);
+    }
+    static boolean halvesAreAlike(String s) {
+        int counta = 0;
+        int countb = 0;
+        for (int i = (int) ((s.length()-1)/2); i >= 0; i--) {
+            char ch = s. charAt(i);
+            if(ch == 'a' || ch =='e'|| ch == 'i' || ch =='o'|| ch =='u'||ch == 'A'||ch == 'E'|| ch =='I'|| ch =='O'||ch == 'U'){
+                counta++;
+            }
+        }
+        for (int i = (int) (((s.length()-1)/2)+1); i < s.length(); i++) {
+            char ch = s. charAt(i);
+            if(ch == 'a' || ch =='e'|| ch == 'i' || ch =='o'|| ch =='u'||ch == 'A'||ch == 'E'|| ch =='I'|| ch =='O'||ch == 'U'){
+                countb++;
+            }
+        }
+        if(counta == countb){
+            return true;
+        } else{
+            return false;
+        }
+    }
+}

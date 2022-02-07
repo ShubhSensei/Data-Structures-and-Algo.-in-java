014ac1cbd9a88b7ebccbe71b0aa14869e9cfca09
Shubham Bansal <bansal4995@gmail.com>
2022-02-07 22:27:28 +0530

    String Questions

--- Strings/E_12.java
+++ Strings/E_12.java
@@ -4,10 +4,29 @@ public class E_12 {
     public static void main(String[] args) {
         String s = "Let's take LeetCode contest";
         char[] ch = s.toCharArray();
-        StringBuilder ans = new StringBuilder();
-        for (int i = ch.length-1; i >= 0; i--) {
-            ans.append(ch[i]);
+        // for (int i = 0; i < ch.length; i++) {
+        //     for (int j = 0; j < ch.length; j++) {
+        //         if(ch[j] == ' ' || j == ch.length-1){
+        //             int start = 0;  int end = j-1;
+        //             if(j == ch.length){
+        //                 end = j;
+        //             }
+        //             while(start>end){
+        //                 char temp = ch[start];
+        //                 ch[start] = ch[end];
+        //                 ch[end] = temp;
+        //                 end--;
+        //                 start++;
+        //             } 
+        //         }
+        //         i = j;
+        //     }
+            
+        // }
+        for (char c : ch) {
+            System.out.print(c + " ");
         }
-        System.out.println(ans.toString());
+        System.out.println(s);
+        // System.out.println(ch.toString());
     }
 }

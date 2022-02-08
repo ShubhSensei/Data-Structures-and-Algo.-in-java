88c835c59796e7e17dcbb1f7de9de733bf6c6ee3
Shubham Bansal <bansal4995@gmail.com>
2022-02-08 17:50:08 +0530

    String Questions

---
+++ E_16.java
@@ -0,0 +1,17 @@
+package Assignments;
+// https://leetcode.com/problems/valid-palindrome/
+public class E_16 {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        String ans = s.replaceAll("[^a-zA-Z0-9]", ""). toLowerCase();
+        String reverse = "";
+        for (int i = ans.length()-1; i >= 0; i--) {
+            reverse = reverse + ans. charAt(i);
+        }
+        if(reverse.equals(ans)){
+            System.out.println(true);
+        } else{
+            System.out.println(false);
+        }
+    }
+}

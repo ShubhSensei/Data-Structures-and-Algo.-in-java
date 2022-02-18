3a44c037074c2c894a66bd12c7cb829ae518c5d9
Shubham Bansal <bansal4995@gmail.com>
2022-02-18 22:42:16 +0530

    Pattern Questions

--- Patterns/Q18.java
+++ Patterns/Q18.java
@@ -1,22 +1,10 @@
 package Assignments.Patterns;
 
-public class Q19 {
+public class Q18 {
     public static void main(String[] args) {
         q(5);
     }
     static void q(int n){
-        for (int row = 1; row < n; row++) {
-            for (int col = 1; col <= row; col++) {
-                System.out.print("*");
-            }
-            for (int space = (n-row)*2; space > 0; space--) {
-                System.out.print(" ");
-            }
-            for (int col = 1; col <= row; col++) {
-                System.out.print("*");
-            } 
-            System.out.println();
-        }
         for (int row = n; row > 0; row--) {
             for (int col = 1; col <= row; col++) {
                 System.out.print("*");
@@ -29,6 +17,17 @@ public class Q19 {
             } 
             System.out.println();
         }
-
-    }   
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }
+            for (int space = (n-row)*2; space > 0; space--) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            } 
+            System.out.println();
+        }
+     }  
 }

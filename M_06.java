a13be906dec2db1ff8edb96c8f4e68ed3c4a1e13
Shubham Bansal <bansal4995@gmail.com>
2022-02-21 23:59:25 +0530

    String Questions

---
+++ M_06.java
@@ -0,0 +1,25 @@
+package Assignments;
+
+public class M_06 {
+    public static void main(String[] args) {
+        int n = 53;
+    }
+    static int nextGreaterElement(int n) {
+        StringBuilder number = new StringBuilder();
+        while(n>0){
+            int N = n%10;
+            n = n/10;
+            number.append(N);
+        }
+        for (int i = number.length()-1; i > 0; i--) {
+            int s = number.charAt(i);
+            int end = number.charAt(i-1);
+            if(s>end){
+                break;
+            } else{
+                System.out.println(-1);
+            }
+        }
+        
+    }
+}
---
+++ M_08.java
@@ -0,0 +1,49 @@
+package Assignments;
+
+public class M_08 {
+    public static void main(String[] args) {
+        
+    }
+    static boolean canTransform(String start, String end) {
+        StringBuilder str = new StringBuilder();
+        char first = ' ';
+        char second = ' ';
+        for (int i = 0; i < start.length()-1; i++) {
+            if(start.length()%2 != 0){
+                if(i == start.length() - 3){
+                    first = start.charAt(i);
+                    second = start.charAt(i+1);
+                    if(first == 'X' && second == 'L'){
+                        str.append("LX");
+                        str.append(start.charAt(i+2));
+                        if(str.toString() == end){
+                            return true;
+                        } else{
+                            return false;
+                        }
+                    } else if( first == 'R' && second == 'X') {
+                        str.append("XR");
+                    } else{
+                        return false;
+                    }
+                }
+            } else{
+                first = start.charAt(i);
+                second = start.charAt(i+1);
+                if(first == 'X' && second == 'L'){
+                    str.append("LX");
+                } else if( first == 'R' && second == 'X') {
+                    str.append("XR");
+                } else{
+                    return false;
+                }
+            }
+        }
+
+        if(str.toString() == end){
+            return true;
+        } else{
+            return false;
+        }
+    }
+}
